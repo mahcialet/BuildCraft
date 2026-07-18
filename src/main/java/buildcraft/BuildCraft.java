65b8732c93b33a89969544ca42ff8bcd0b95f7cc
mahcialet <177428187+mahcialet@users.noreply.github.com>
2026-07-18 21:30:04 +0900

    Bootstrap NeoForge 26.1.2 build

---
+++ src/main/java/buildcraft/BuildCraft.java
@@ -0,0 +1,12 @@
+package buildcraft;
+
+import net.neoforged.fml.common.Mod;
+
+/** Entry point for the incremental NeoForge port. */
+@Mod(BuildCraft.MOD_ID)
+public final class BuildCraft {
+    public static final String MOD_ID = "buildcraft";
+
+    public BuildCraft() {
+    }
+}
